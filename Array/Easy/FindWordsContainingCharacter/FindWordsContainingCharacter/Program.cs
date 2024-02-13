72396eb3a31c076564da1a782679fd8abac2b8d8
Emir0zcelik <themirozcelik@gmail.com>
2024-02-13 17:24:41 +0300

    FindWordsContainingCharacter

---
+++ Array/Easy/FindWordsContainingCharacter/FindWordsContainingCharacter/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        IList<int> ans = new List<int>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words[i].Length; j++)
+            {
+                if (words[i].Contains(x))
+                {
+                    ans.Add(i);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
