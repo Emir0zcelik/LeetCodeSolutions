ad9d87e9d3d76a1a0129ff54c49ebb8f8b0aebf6
Emir0zcelik <themirozcelik@gmail.com>
2024-02-13 17:17:17 +0300

    ShuffleTheArray Solution

---
+++ Array/Easy/ShuffleTheArray/ShuffleTheArray/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int[] Shuffle(int[] nums, int n)
+    {
+        int[] ans = new int[nums.Length];
+        int j = 0;
+        int k = n;
+        for (int i = 0; i < 2 * n; i++)
+        {
+            ans[i] = (i % 2 == 0) ? nums[j++] : nums[k++];
+        }
+
+        return ans;
+    }
+}
