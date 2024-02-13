3f75ea705c12ddb06632267955dd9fb45db59a83
Emir0zcelik <themirozcelik@gmail.com>
2024-02-13 05:09:49 +0300

    ConcatenationOfArray Solution

---
+++ Array/Easy/ConcatenationOfArray/ConcatenationOfArray/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] ans = new int[nums.Length * 2];
+        int j = 0;
+        for (int i = 0; i < ans.Length; i++)
+        {
+            if (i < nums.Length)
+            {
+                ans[i] = nums[i];
+            }
+            else
+            {
+                ans[i] = ans[j++];
+            }
+        }
+
+        return ans;
+    }
+}
