dcc0459507befc3565db5b0c728f32df9e7c7ab1
Emir0zcelik <themirozcelik@gmail.com>
2024-02-13 05:30:52 +0300

    FinalValueOfVariableAfterPerformingOperations Solution

---
+++ Array/Easy/FinalValueOfVariableAfterPerformingOperations/FinalValueOfVariableAfterPerformingOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < operations.Length; i++)
+        {
+            if (operations[i].Contains("++"))
+            {
+                sum++;
+            }
+            if (operations[i].Contains("--"))
+            {
+                sum--;
+            }
+        }
+
+        return sum;
+    }
+}
