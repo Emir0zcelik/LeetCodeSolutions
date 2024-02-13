58c51912031c561ec64e8fa322c90bdea7741d0d
Emir0zcelik <themirozcelik@gmail.com>
2024-02-13 05:22:25 +0300

    NumberOfGoodPairs Solution

---
+++ Array/Easy/NumberOfGoodPairs/NumberOfGoodPairs/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int NumIdenticalPairs(int[] nums)
+    {
+        int pairs = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    pairs++;
+                }
+            }
+        }
+
+        return pairs;
+    }
+}
