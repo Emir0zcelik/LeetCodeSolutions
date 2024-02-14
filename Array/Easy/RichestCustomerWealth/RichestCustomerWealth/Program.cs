dc40a744e2583d1c146eb0ecd6880006c13ae7e6
Emir0zcelik <themirozcelik@gmail.com>
2024-02-14 03:11:21 +0300

    RichestCustomerWealth Solution

---
+++ Array/Easy/RichestCustomerWealth/RichestCustomerWealth/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public int MaximumWealth(int[][] accounts)
+    {
+        int ans = 0;
+
+        int[] sum = new int[accounts.GetLength(0)];
+
+        for (int i = 0; i < accounts.Length; i++)
+        {
+            sum[i] = 0;
+            for (int j = 0; j < accounts[i].Length; j++)
+            {
+                sum[i] += accounts[i][j];
+            }
+        }
+
+        for (int i = 0; i < sum.Length; i++)
+        {
+            if (sum[i] >= ans)
+            {
+                ans = sum[i];
+            }
+        }
+        return ans;
+    }
+}
