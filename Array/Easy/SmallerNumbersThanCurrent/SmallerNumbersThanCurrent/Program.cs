e45b338824f0642e82204d3dd10abb8d87e1991c
Emir0zcelik <themirozcelik@gmail.com>
2024-02-15 06:40:56 +0300

    Solution

---
+++ Array/Easy/SmallerNumbersThanCurrent/SmallerNumbersThanCurrent/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+        int[] answer = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i != j)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        answer[i]++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
