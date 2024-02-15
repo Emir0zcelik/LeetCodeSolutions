74a19b2434d2b03c14ac1c3a584524a95f8be7ed
Emir0zcelik <themirozcelik@gmail.com>
2024-02-15 06:34:13 +0300

    Solution

---
+++ Array/Easy/RunningSumOf1DArray/RunningSumOf1DArray/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int[] RunningSum(int[] nums)
+    {
+        int temp = 0;
+        int[] answer = new int[nums.Length];
+        int i = 0;
+        
+        foreach (var num in nums)
+        {
+            temp += num;
+            answer[i++] = temp;
+        }
+
+        return answer;
+    }
+}
