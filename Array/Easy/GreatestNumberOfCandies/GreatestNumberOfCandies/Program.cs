2acaa5cc4d5150958d8671b6251820a9287f2c28
Emir0zcelik <themirozcelik@gmail.com>
2024-02-15 06:11:37 +0300

    NumberOfCandies Solution

---
+++ Array/Easy/GreatestNumberOfCandies/GreatestNumberOfCandies/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        List<bool> answer = new List<bool>();
+
+        int max = 0;
+        
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (candies[i] > max)
+            {
+                max = candies[i];
+            }
+        }
+
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (candies[i] + extraCandies >= max)
+            {
+                answer.Insert(i, true);
+            }
+            else
+            {
+                answer.Insert(i, false);
+            }
+        }
+
+        return answer;
+    }
+}
