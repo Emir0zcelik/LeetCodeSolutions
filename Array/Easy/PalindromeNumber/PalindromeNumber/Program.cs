1d1da1c1dd3c96f164e3e7d4fcfe38a6eb1a6a68
Emir0zcelik <themirozcelik@gmail.com>
2024-02-15 05:55:09 +0300

    PalindromeNumber Solution

---
+++ Array/Easy/PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        int temp = x;
+        int reverse = 0;
+
+        while (temp > 0)
+        {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        
+        return x == reverse;
+    }
+}
---
+++ Array/Easy/ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public bool IsValid(string s)
+    {
+        Stack<string> stack;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            
+        }
+    }
+}
