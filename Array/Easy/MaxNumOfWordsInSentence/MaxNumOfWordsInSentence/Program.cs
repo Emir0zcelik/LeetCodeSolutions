38889a5e5754daa072d29952516e9b925a70c3ef
Emir0zcelik <themirozcelik@gmail.com>
2024-02-15 07:13:44 +0300

    Solution

---
+++ Array/Easy/MaxNumOfWordsInSentence/MaxNumOfWordsInSentence/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int MostWordsFound(string[] sentences)
+    {
+        int max = 0;
+
+        foreach (string sentence in sentences)
+        {
+            int count = sentence.Split(' ').Length;
+            max = Math.Max(max, count);
+        }
+
+        return max;
+    }
+}
