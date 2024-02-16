b26d17a9dee495711de0d542179082e0d69a3cdc
Emir0zcelik <themirozcelik@gmail.com>
2024-02-16 09:03:42 +0300

    Solution

---
+++ Array/Easy/SumIndicesWithKSetBits/SumIndicesWithKSetBits/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution {
+    public int SumIndicesWithKSetBits(IList<int> nums, int k) {
+        IList<int> indices = new List<int>();
+        int sum = 0;
+        string binary;
+        int count = 0;
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            binary = Convert.ToString(i, 2);
+            foreach (char bit in binary)
+            {
+                if (bit == '1')
+                {
+                    count++;
+                }
+            }
+
+            if (count == k)
+            {
+                
+                sum += nums[i];
+            }
+            count = 0;
+        }
+        
+        return sum;
+    }
+}
